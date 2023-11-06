de5dea22580034a0243f684f827a0829b1493939
Adem Hatay <hatayadem5@gmail.com>
2023-11-07 02:42:39 +0300

    sidebar ui completed.

--- app/page/app.php
+++ app/page/app.php
@@ -14,7 +14,6 @@ if (isset($_POST['logout'])) {
     exit();
 }
 
-
 ?>
 
 
@@ -24,13 +23,53 @@ if (isset($_POST['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App | Kamagru</title>
+    <link rel="stylesheet" href="/assets/style/app.css">
 </head>
 <body>
 
-    <form action="" method="post">
-        <button type="submit" name="logout">Logout</button>
-    </form>
+   <div class="container">
+    <header class="cont_item">
+        <div class="logo">
+            <a href="/page/app.php">Kamagru</a>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/page/app.php">
+                        <img src="/assets/icons/home-outlined.png" alt="home">
+                        Home
+                    </a>
+                </li>
+                <li><a href="/page/app.php">
+                <img src="/assets/icons/search-outlined.png" alt="search">
+                    Search
+                </a></li>
+                <li><a href="/page/app.php">
+                    <img src="/assets/icons/notification-outlined.png" alt="notification">
+                    Notifications
+                </a></li>
+                <li><a href="/page/app.php">
+                    <img src="/assets/icons/create-outlined.png" alt="create">
+                    Create
+                </a></li>
+                <li><a href="/page/app.php">
+                    <img src="/assets/icons/account.png" alt="profile">
+                    Profile</a></li>
+            </ul>
+        </nav>
+        <form action="" method="post" class="logout">
+            <button type="submit" class="btn" name="logout">Logout</button>
+        </form> 
+    </header>
 
+        <main class="cont_item main">
+            2
+        </main>
+
+        <aside class="cont_item">
+            3
+        </aside>
+   </div>
 
 </body>
 </html>
